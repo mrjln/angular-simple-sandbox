019667e698540f690bd33753e48f129a6d7ae5db
mrjln <marjolein_tromp@hotmail.com>
2017-12-06 15:25:46 +0100

    removing items from list with animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,22 @@ import { trigger, style, transition, animate, keyframes, query, stagger} from '@
   animations: [
     trigger('goals', [
       //this is were the animation specific function will reside
-      transition('*=>*',[
+      transition('* => *',[
         query(':enter', style({ opacity:0 }), { optional: true }),
+
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
             style({opacity: 0, transform:'translateY(-75%)', offset: 0}),
             style({opacity: 0.5, transform:'translateY(35px)', offset: .3}),
             style({opacity: 1, transform:'translateY(0)', offset: 1}),
-          ]))
-        ]), { optional: true })
+          ]))]), {optional: true }),
+
+          query(':leave', stagger('300ms', [
+            animate('.6s ease-in', keyframes([
+              style({opacity: 1, transform:'translateY(0)', offset: 0}),
+              style({opacity: 0.5, transform:'translateY(35px)', offset: .3}),
+              style({opacity: 0, transform:'translateY(-75%)', offset: 1}),
+            ]))]), {optional: true })
       ])
     ])
   ]
@@ -23,7 +30,7 @@ import { trigger, style, transition, animate, keyframes, query, stagger} from '@
 export class HomeComponent implements OnInit {
 itemCount: number;
 goalText: string = 'My first lifegoal';
-goals = [];
+goals = ["goal 1", "goal 2"];
 
 
   constructor() { }
@@ -38,6 +45,10 @@ goals = [];
     this.goalText= "";
     //clear out the input after it is submitted
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1);
   }
 
 }
